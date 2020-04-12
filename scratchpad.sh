2564957c3ebb70a7397206d597e0d6e58092cebb
Tore Sinding Bekkedal <toresbe@gmail.com>
2020-04-12 15:32:07 +0200

    add basic infrastructure

---
+++ scratchpad.sh
@@ -0,0 +1,8 @@
+kubectl create secret docker-registry regcred --docker-server=https://docker.pkg.github.com --docker-username=toresbe --docker-password=$(cat ~/GITHUB_PACKAGE_TOKEN) --docker-email=toresbe@gmail.com
+
+
+docker build -t docker.pkg.github.com/toresbe/docker_experiments/frontend:0.0.1 ~/frikanalen/packages/frontend/
+docker push docker.pkg.github.com/toresbe/docker_experiments/frontend:0.0.1
+
+helm repo add traefik https://containous.github.io/traefik-helm-chart
+
